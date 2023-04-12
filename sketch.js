fef15b513e40f89cce938b76069c24fa4f624c39
ThoVig <thoger.hansen@gmail.com>
2023-04-12 10:55:39 +0200

    sdfgfd

--- sketch.js
+++ sketch.js
@@ -1,9 +1,17 @@
+let x = 0;
+let y = [];
+var data;
 function setup() 
 {
-  createCanvas(400, 400);
+  createCanvas(800, 300);
+  rectMode(CENTER);
 }
 
 function draw() 
 {
   background(220);
+  fill(0);
+  rect(800/2, 300, 810, 200);
+  fill(255);
+  rect(400,190,20,20)
 }
